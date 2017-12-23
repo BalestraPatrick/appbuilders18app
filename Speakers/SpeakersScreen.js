9229e8c334aa772990c6cbecd9104600ff67bf69
Patrick Balestra <me@patrickbalestra.com>
2017-12-23 15:54:37 +0100

    Add dummy data in speakers screen

--- Speakers/SpeakersScreen.js
+++ Speakers/SpeakersScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image, AppRegistry, SectionList } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
@@ -18,17 +18,41 @@ export default class SpeakersScreen extends React.Component {
 
   render() {
     return (
-      <SafeAreaView style={styles.safeArea}>
-        <Button
-          onPress={() => this.props.navigation.goBack()}
-          title="Go back"
-        />
-      </SafeAreaView>
+        <View style={styles.container}>
+          <SectionList
+            sections={[
+              {title: 'D', data: ['Devin']},
+              {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 0,
+   backgroundColor: 'white'
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
   safeArea: {
     flex: 1,
     backgroundColor: '#ffffff',
