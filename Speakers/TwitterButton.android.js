1506db1407f0e91c230e5cbf37f8069caf588062
Jonas Schmid <jonas.schmid@gmail.com>
2018-04-06 17:57:15 +0200

    Fixed Twitter link on Android

--- Speakers/TwitterButton.android.js
+++ Speakers/TwitterButton.android.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, Linking } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export const TwitterButton = () => 
+export const TwitterButton = (props) => 
     <Ionicons 
         color="#e91e63" 
         name="logo-twitter" 
         size={30}
         style={styles.twitterButton}
         onPress={() => 
-            Linking.openURL(`https://twitter.com/${speaker.twitter}`)
+            Linking.openURL(`https://twitter.com/${props.speaker}`)
         } 
     />
 
